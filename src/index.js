cdf89b64854fd3c44622cc47579bd3cb51ec8b90
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-10-23 18:40:07 -0500

    Created actions, created reducer file. Created initial state for our car.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//redux / react-redux imports
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+//reducer import
+import { reducer } from './reducers/reducers'
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, rootElement);
